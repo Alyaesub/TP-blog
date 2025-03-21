e924948dad6ac1c11a6bcf5a99fc57eaf0de890b
Alyaesub <reynierpascal@yahoo.com>
2025-03-21 07:05:25 +0100

    first-commit

---
+++ README.md
@@ -0,0 +1,9 @@
+création d'un blog:
+
+en php vanilla,
+
+-Page listing d'articles (pagination)
+-Page listing d'articles pour une catégorie (pagination)
+-Page présentation d'un article
+-Administration des catégories
+-Administration des articles
---
+++ public/index.php
@@ -0,0 +1,20 @@
+<h1>ceci est mon routeur</h1>
+<?php
+
+require '../vendor/autoload.php';
+
+$router = new AltoRouter();
+
+
+$router->map('GET', '/blog', function () {
+  require dirname(__DIR__) . '/views/post/index.php';
+});
+$router->map('GET', '/blog/categories', function () {
+  require dirname(__DIR__) . '/views/category/show.php';
+});
+$match = $router->match();
+if ($match) {
+  call_user_func_array($match['target'], $match['params']);
+} else {
+  echo "Page introuvable";
+}
