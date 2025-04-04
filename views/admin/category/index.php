40cc31930567074a4409074012778ca420c71207
Alyaesub <dadz13104@gmail.com>
2025-04-04 04:13:30 -0700

    add route dans indexAdmin

--- views/admin/category/index.php
+++ views/admin/category/index.php
@@ -10,10 +10,35 @@ $title = "Gestion des catégories";
 
 <div class="container">
   <h1>Administration des catégories</h1>
+  <!-- Bouton pour créer une nouvelle catégorie -->
+  <div class="d-flex justify-content-between my-4">
+    <h2>Liste des catégories</h2>
+    <a href="<?= $router->url('admin_category_new') ?>" class="btn btn-primary">Nouvelle catégorie</a>
+  </div>
   <table class="table">
     <thead>
       <tr>
         <th>ID</th>
         <th>Nom</th>
-
+        <th>Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php foreach ($categories as $category): ?>
+        <tr>
+          <td>#<?= $category->getId() ?></td>
+          <td><?= htmlentities($category->getName()) ?></td>
+          <td>
+            <a href="<?= $router->url('admin_category_edit', ['id' => $category->getId()]) ?>" class="btn btn-primary btn-sm">
+              Éditer
+            </a>
+            <form action="<?= $router->url('admin_category_delete', ['id' => $category->getId()]) ?>" method="POST"
+              style="display: inline" onsubmit="return confirm('Êtes-vous sûr de vouloir supprimer cette catégorie ?')">
+              <button type="submit" class="btn btn-danger btn-sm">Supprimer</button>
+            </form>
+          </td>
+        </tr>
+      <?php endforeach ?>
+    </tbody>
+  </table>
 </div>
